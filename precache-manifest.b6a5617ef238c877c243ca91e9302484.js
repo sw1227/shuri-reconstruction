48a8d6b3bb2047f4d6600c1bf2ed12562dc9689c
sw1227 <32032108+sw1227@users.noreply.github.com>
2019-11-16 13:38:10 +0900

    First commit

---
+++ precache-manifest.b6a5617ef238c877c243ca91e9302484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef531a7fbf51c724c31bb37cce9e8e8c",
+    "url": "/shuri-reconstruction/index.html"
+  },
+  {
+    "revision": "d75c4c1249d2af656e9a",
+    "url": "/shuri-reconstruction/static/css/main.db7ff91b.chunk.css"
+  },
+  {
+    "revision": "9cb0992d9cffd1276b21",
+    "url": "/shuri-reconstruction/static/js/2.14e98d94.chunk.js"
+  },
+  {
+    "revision": "d75c4c1249d2af656e9a",
+    "url": "/shuri-reconstruction/static/js/main.942c146d.chunk.js"
+  },
+  {
+    "revision": "0da8667a4392553eeb8b",
+    "url": "/shuri-reconstruction/static/js/runtime~main.c8d3cced.js"
+  },
+  {
+    "revision": "a9e302a611acb05138b87a6d627e1024",
+    "url": "/shuri-reconstruction/static/media/shuri.a9e302a6.glb"
+  }
+]);
